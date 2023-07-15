0c864df34a35b87b44f7770007ffadf4690bdf28
gsarun125 <127116889+gsarun125@users.noreply.github.com>
2023-07-15 22:36:31 +0530

    working

--- app/src/main/java/com/mini/remainder/Activity/HomeActivity.java
+++ app/src/main/java/com/mini/remainder/Activity/HomeActivity.java
@@ -1,23 +1,29 @@
 package com.mini.remainder.Activity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.mini.remainder.R;
+import com.mini.remainder.model.GridAdapter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class HomeActivity extends MainActivity {
 
-    ListView l;
-    List<String> a = new ArrayList();
+    GridView gridView;
+    List<String> a_title = new ArrayList();
+    List<String> b_title=new ArrayList();
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -25,29 +31,40 @@ public class HomeActivity extends MainActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         FloatingActionButton add = (FloatingActionButton) findViewById(R.id.add);
-        l = findViewById(R.id.list);
+        gridView = findViewById(R.id.list);
 
 
         Cursor c1 = getDb().getData();
         if (c1.moveToFirst()) {
-            do {
-                StringBuilder sb = new StringBuilder();
-                int columnsQty = c1.getColumnCount();
-                for (int i = 0; i < columnsQty; ++i) {
-                    sb.append(c1.getString(i));
-                    if (i < columnsQty - 1)
-                        sb.append("  ");
-                }
-                a.add(sb.toString());
-            } while (c1.moveToNext());
+                do{
+                    @SuppressLint("Range") String data = c1.getString(c1.getColumnIndex("title"));
+                    @SuppressLint("Range") String data1 = c1.getString(c1.getColumnIndex("date"));
+                    a_title.add(data);
+                    b_title.add(data1);
+
+                }while(c1.moveToNext());
+
         }
 
+        GridAdapter gridAdapter=new GridAdapter(HomeActivity.this,a_title,b_title);
+        gridView.setAdapter(gridAdapter);
+        ImageView a=(ImageView)findViewById(R.id.image_empty) ;
+        gridView.setEmptyView(a);
 
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, a);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
+           /* ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, a);
             l.setAdapter(arrayAdapter);
-           ImageView a=(ImageView)findViewById(R.id.image_empty) ;
+            ImageView a=(ImageView)findViewById(R.id.image_empty) ;
             l.setEmptyView(a);
 
+            */
 
             add.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -57,7 +74,5 @@ public class HomeActivity extends MainActivity {
                     startActivity(i);
                 }
             });
-
-
     }
 }
